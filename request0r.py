ab51513882b4f045ad3cb37eb7097fa0b3cb588b
Tomasinjo <43334457+Tomasinjo@users.noreply.github.com>
2022-04-26 09:28:28 +0200

    Create request0r.py

---
+++ request0r.py
@@ -0,0 +1,24 @@
+import requests
+import datetime
+import time
+
+urls = ['https://www.domain1.com/url', 'https://www.domain2.com/url', 'https://www.domain2.com/url']
+
+def check_urls(url):
+    timestamp = datetime.datetime.now().isoformat()
+    status = ''
+    response_time = ''
+    exception = ''
+    try:
+        resp = requests.get(url, timeout=10)
+    except Exception as exception:
+        return timestamp, url, status, response_time, exception
+    status = resp.status_code
+    response_time = resp.elapsed.total_seconds()
+    return timestamp, url, status, response_time, exception
+
+while True:
+    for url in urls:
+        timestamp, url, status, response_time, exception = check_urls(url)
+        print(f'{timestamp}|{url}|{status}|{response_time}|{exception}')
+    time.sleep(5)
